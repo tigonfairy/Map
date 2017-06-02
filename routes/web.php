e41fa656bb2b91f8ea4b878208dff8523fdc404a
huyth <trinhhuyk57@gmail.com>
2017-06-02 13:56:37 -0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+
 Route::get('language/{locale}', function ($locale) {
     Session::put('locale', $locale);
     return redirect()->back();
