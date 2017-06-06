fc1a4526ec707b174ab773bf9c921ad4aa0048d6
hin keu <hung.tt@teko.vn>
2017-06-06 15:53:19 +0700

    add full screen

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -81,7 +81,7 @@
             display: none !important;
         }
     </style>
-
+@stack('style_head')
 </head>
 <!-- END HEAD -->
 
@@ -270,7 +270,7 @@
 <script src="/assets/global/plugins/jquery-minicolors/jquery.minicolors.min.js" type="text/javascript"></script>
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN THEME GLOBAL SCRIPTS -->
-<script src="/assets/global/scripts/app.min.js" type="text/javascript"></script>
+
 <!-- END THEME GLOBAL SCRIPTS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
 <script src="/assets/pages/scripts/components-color-pickers.min.js" type="text/javascript"></script>
