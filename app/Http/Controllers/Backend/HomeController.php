71549836a4a9b9cc56c9dfe40b560ad496bbd77f
hin keu <hung.tt@teko.vn>
2017-06-14 13:58:07 +0700

    Update chart Dashboard

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -17,20 +17,33 @@ class HomeController extends AdminController
     }
     public function dashboard(Request $request){
         $user = auth()->user();
-        $month = '01-2017';
-        $areaIds = $user->area()->get()->pluck('id')->toArray();
+        $area = $user->area()->get()->pluck('id')->toArray();
+        $subArea = Area::whereIn('parent_id',$area)->get()->pluck('id')->toArray();
+        $areaIds = array_unique(array_merge($area,$subArea));
         $agents = Agent::whereIn('area_id',$areaIds)->get()->pluck('id')->toArray();
-        $subAgent =
+        $year = Carbon::now()->year;
         //agent Id of user
         $agentId = $user->agent()->get()->pluck('id')->toArray();
-//        $agentId= array_unique(array_merge($agentId,$agents));
+        $agentId= array_unique(array_merge($agentId,$agents));
 
-//        $products = DB::table('sale_agents')
-//            ->select(\DB::raw('SUM(sales_plan) as sales_plan,SUM(sales_real) as sales_real,product_id,products.name'))
-//            ->whereIn('agent_id',$agentId)->groupBy('month')->where('month','like','%2017%')
-//            ->join('products','sale_agents.product_id','=','products.id')
-//            ->get();
-//        dd($products);
-        return view('admin.dashboard', compact('month'));
+        $products = DB::table('sale_agents')
+            ->select(\DB::raw('SUM(sales_plan) as sales_plan,SUM(sales_real) as sales_real,month'))
+            ->whereIn('agent_id',$agentId)->groupBy('month')->where('month','like','%'.$year.'%')->orderBy('month')
+            ->get()->toArray();
+        $sales_plan = [];
+        $sales_real = [];
+
+
+        for($i = 0;$i < 12;$i++){
+            $sales_plan[$i] = 0;
+            $sales_real[$i] = 0;
+        }
+
+        foreach ($products as $key => $product){
+            $sales_plan[$key] = intval($product->sales_plan);
+            $sales_real[$key] = intval($product->sales_real);
+
+        }
+        return view('admin.dashboard', compact('month','sales_plan','sales_plan','sales_real'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -114,7 +114,7 @@
             tooltip: {
                 headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
                 pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                '<td style="padding:0"><b>{point.y} </b></td></tr>',
                 footerFormat: '</table>',
                 shared: true,
                 useHTML: true
@@ -127,11 +127,12 @@
             },
             series: [{
                 name: 'DTKH',
-                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+//                data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+                data: {{json_encode($sales_plan)}}
 
             }, {
                 name: 'DTTT',
-                data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+                data: {{json_encode($sales_real)}}
 
             }]
         });
