0360a7e045ceeeec58d91b76deba91d5f9a3ad9c
Mr Hin Keu <Mr Hin Keu>
2017-06-16 00:27:03 +0700

    custom dashboard 1 2

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::group(['middleware' => ['auth','language'],
     });
 
     Route::get('/', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
+    Route::post('/d', ['as' => 'chart', 'uses' => 'HomeController@chartDashboard']);
+
     Route::get('/admin', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
     Route::post('/', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
     Route::post('/admin', ['as' => 'dashboard', 'uses' => 'HomeController@dashboard']);
