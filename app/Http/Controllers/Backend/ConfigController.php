f245d9bfe5197eb98d3d8dbdc2a15fac6c00e29f
huyth <trinhhuyk57@gmail.com>
2017-06-16 23:29:25 +0700

    update

--- app/Http/Controllers/Backend/ConfigController.php
+++ app/Http/Controllers/Backend/ConfigController.php
@@ -9,7 +9,7 @@ class ConfigController extends Controller
 {
     public function index(Request $request)
     {
-        if (auth()->user()->roles->first()['id'] != 1) {
+        if (auth()->user()->roles->first()['email'] != 'admin@gamil.com') {
             abort(403);
         }
         $config = [];
@@ -21,7 +21,7 @@ class ConfigController extends Controller
 
     public function store(Request $request)
     {
-        if (auth()->user()->roles->first()['id'] != 1) {
+        if (auth()->user()->roles->first()['email'] != 'admin@gamil.com') {
             abort(403);
         }
         $this->validate($request,[
