8d2537cf69d3ebec39c9cc153fd9a5a3cfcde7da
huyth <trinhhuyk57@gmail.com>
2017-06-19 19:09:12 +0700

    update code

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -14,6 +14,7 @@ return [
     */
 
     'failed' => 'These credentials do not match our records.',
+    'blocked' => 'Your account is blocked.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
