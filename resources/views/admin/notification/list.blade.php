3710c6dc3ac8e32a380a9a6144bee1efe35585e8
Hin <hunguet1471994@gmail.com>
2017-08-14 14:32:05 +0700

    UP

--- resources/views/admin/notification/list.blade.php
+++ resources/views/admin/notification/list.blade.php
@@ -4,9 +4,9 @@
     <!-- Page header -->
     <div class="page-header">
         <div class="page-header-content">
-            <div class="page-title">
-                <h2>Danh sách thông báo</h2>
-            </div>
+            {{--<div class="page-title">--}}
+                {{--<h2>Danh sách thông báo</h2>--}}
+            {{--</div>--}}
 
             <div class="heading-elements">
                 <div class="heading-btn-group">
@@ -26,104 +26,27 @@
                     <div class="portlet-title">
                         <div class="caption caption-md">
                             <i class="icon-bar-chart theme-font hide"></i>
-                            <span class="caption-subject font-blue-madison bold uppercase">Customer Support</span>
+                            <span class="caption-subject font-blue-madison bold uppercase">Danh sách thông báo</span>
                             <span class="caption-helper">45 pending</span>
                         </div>
-                        <div class="inputs">
-                            <div class="portlet-input input-inline input-small ">
-                                <div class="input-icon right">
-                                    <i class="icon-magnifier"></i>
-                                    <input type="text" class="form-control form-control-solid" placeholder="search..."> </div>
-                            </div>
-                        </div>
+
                     </div>
                     <div class="portlet-body">
-                        <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 305px;"><div class="scroller" style="height: 305px; overflow: hidden; width: auto;" data-always-visible="1" data-rail-visible1="0" data-handle-color="#D7DCE2" data-initialized="1">
+                        <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; "><div class="scroller" style=" overflow: hidden; width: auto;" data-always-visible="1" data-rail-visible1="0" data-handle-color="#D7DCE2" data-initialized="1">
                                 <div class="general-item-list">
+                                    @foreach($notifications as $notification)
                                     <div class="item">
                                         <div class="item-head">
                                             <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar4.jpg">
-                                                <a href="#" class="item-name primary-link">Nick Larson</a>
-                                                <span class="item-label">3 hrs ago</span>
+                                                <span class="item-label">{{$notification->created_at->format('s:i:H d/m/Y')}}</span>
                                             </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-success"></span> Open</span>
+
                                         </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </div>
+                                        <div class="item-body">{{$notification->title}} </div>
                                     </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar3.jpg">
-                                                <a href="#" class="item-name primary-link">Mark</a>
-                                                <span class="item-label">5 hrs ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-warning"></span> Pending</span>
-                                        </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat tincidunt ut laoreet. </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar6.jpg">
-                                                <a href="#" class="item-name primary-link">Nick Larson</a>
-                                                <span class="item-label">8 hrs ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-primary"></span> Closed</span>
-                                        </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh. </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar7.jpg">
-                                                <a href="#" class="item-name primary-link">Nick Larson</a>
-                                                <span class="item-label">12 hrs ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-danger"></span> Pending</span>
-                                        </div>
-                                        <div class="item-body"> Consectetuer adipiscing elit Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar9.jpg">
-                                                <a href="#" class="item-name primary-link">Richard Stone</a>
-                                                <span class="item-label">2 days ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-danger"></span> Open</span>
-                                        </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, ut laoreet dolore magna aliquam erat volutpat. </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar8.jpg">
-                                                <a href="#" class="item-name primary-link">Dan</a>
-                                                <span class="item-label">3 days ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-warning"></span> Pending</span>
-                                        </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </div>
-                                    </div>
-                                    <div class="item">
-                                        <div class="item-head">
-                                            <div class="item-details">
-                                                <img class="item-pic" src="../assets/pages/media/users/avatar2.jpg">
-                                                <a href="#" class="item-name primary-link">Larry</a>
-                                                <span class="item-label">4 hrs ago</span>
-                                            </div>
-                                            <span class="item-status">
-                                                                        <span class="badge badge-empty badge-success"></span> Open</span>
-                                        </div>
-                                        <div class="item-body"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </div>
-                                    </div>
+                                    @endforeach
+
+
                                 </div>
                             </div><div class="slimScrollBar" style="background: rgb(215, 220, 226); width: 7px; position: absolute; top: 12px; opacity: 0.4; display: block; border-radius: 7px; z-index: 99; right: 1px; height: 142.24px;"></div><div class="slimScrollRail" style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(234, 234, 234); opacity: 0.2; z-index: 90; right: 1px;"></div></div>
                     </div>
