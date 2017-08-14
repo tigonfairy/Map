24156b6865a3017d5a10d76a33f53a20544b302d
Hin <hunguet1471994@gmail.com>
2017-08-14 11:19:53 +0700

    UP

--- app/Http/Controllers/Backend/NotificationController.php
+++ app/Http/Controllers/Backend/NotificationController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Notification;
+use Carbon\Carbon;
 class NotificationController extends AdminController
 {
     public function getNotification(Request $request)
     {
 
         $unreadCount = Notification::where('unread', 1)->count();
-        $notifications = Notification::orderBy('createdAt', 'desc');
+        $notifications = Notification::orderBy('created_at', 'desc');
 
         if ($request->has('since')) {
-            $notifications = $notifications->where('createdAt', '>=', Carbon::createFromTimeStamp((int) $request->input('since')));
+            $notifications = $notifications->where('created_at', '>=', Carbon::createFromTimeStamp((int) $request->input('since')));
         } else {
             $notifications = $notifications->take((int) $request->input('limit', 10));
         }
