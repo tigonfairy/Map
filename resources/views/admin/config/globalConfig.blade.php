486b9d791af7957e40fb375d9252430844bd8c03
Hin <hunguet1471994@gmail.com>
2017-08-21 10:04:14 +0700

    UPDATE

--- resources/views/admin/config/globalConfig.blade.php
+++ resources/views/admin/config/globalConfig.blade.php
@@ -23,7 +23,7 @@
                     <div class="panel panel-flat">
                         <div class="panel-body">
                             <form method="POST" action="{{ route('Admin::config@globalConfig')}}">
-                                {{ csrf_field() }}
+                            {{ csrf_field() }}
 
                             <!---------- Agent ID------------>
 
@@ -31,22 +31,22 @@
                                 <div class="form-group">
                                     <table class="table table-striped table-bordered" cellspacing="0" width="100%">
                                         <thead>
-                                            <th>{{trans('position')}}</th>
-                                            <th>{{trans('textColor')}}</th>
-                                            <th>{{trans('fontSize')}}</th>
+                                        <th>{{trans('position')}}</th>
+                                        <th>{{trans('textColor')}}</th>
+                                        <th>{{trans('fontSize')}}</th>
                                         </thead>
 
                                         <tbody>
                                         @foreach(\App\Models\User::$positionTexts as $key => $value)
                                             @php
                                                 $textColor = null;
-                                                         $fontSize = null;
-                                                     $config = \App\Models\Config::where('position_id',$key)->first();
-                                                     if($config) {
-                                                         $textColor = $config->textColor;
-                                                         $fontSize = $config->fontSize;
-                                                     }
-                                                    @endphp
+                                                 $fontSize = null;
+                                             $config = \App\Models\Config::where('position_id',$key)->first();
+                                             if($config) {
+                                                 $textColor = $config->textColor;
+                                                 $fontSize = $config->fontSize;
+                                             }
+                                            @endphp
                                             <tr>
                                                 <td>{{ $value }}</td>
                                                 <td>
@@ -57,15 +57,17 @@
                                                                value="{{$textColor}}"
                                                                name="textColor[{{$key}}]">
                                                         <span class="input-group-btn">
-                                                    <button class="btn default" type="button"><i style="background-color: {{!empty($textColor) ? $textColor : '#3865a8'}};"></i>&nbsp;</button>
+                                                    <button class="btn default" type="button"><i
+                                                                style="background-color: {{!empty($textColor) ? $textColor : '#3865a8'}};"></i>&nbsp;</button>
                                                 </span>
                                                     </div>
 
                                                 </td>
                                                 <td>
                                                     <div class="input-group  form-control">
-                                                    <input type="number"  name="fontSize[{{$key}}]" class="form-control" value="{{$fontSize}}" />
-                                                       </div>
+                                                        <input type="number" name="fontSize[{{$key}}]"
+                                                               class="form-control" value="{{$fontSize}}"/>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -96,12 +98,12 @@
             $(document).ready(function () {
                 $('.agents').select2();
 
-                $('.monthPicker').datepicker( {
+                $('.monthPicker').datepicker({
                     changeMonth: true,
                     changeYear: true,
                     showButtonPanel: true,
                     dateFormat: 'mm-yy',
-                    onClose: function(dateText, inst) {
+                    onClose: function (dateText, inst) {
                         var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
                         var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
                         $(this).datepicker('setDate', new Date(year, month, 1));
