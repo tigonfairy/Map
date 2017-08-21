807a40fe0ffc41d05511f746aefd3e586c9d7e1a
Hin <hunguet1471994@gmail.com>
2017-08-21 09:03:16 +0700

    UPDATE

--- database/migrations/2017_08_21_085919_create_config_table.php
+++ database/migrations/2017_08_21_085919_create_config_table.php
@@ -13,12 +13,14 @@ class CreateConfigTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('configs');
         Schema::create('configs', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
             $table->string('fontSize');
             $table->string('textColor');
             $table->string('background');
+            $table->integer('position_id');
 
         });
     }
