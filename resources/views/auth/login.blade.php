4292020c293c907d32c88a48dd097e94c5c7f1fb
Hin <hunguet1471994@gmail.com>
2018-02-01 10:29:36 +0700

    UP

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,6 @@
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
-<!-- BEGIN HEAD -->
-
-
-<!-- Mirrored from keenthemes.com/preview/metronic/theme/admin_1_material_design/page_user_login_6.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Nov 2016 04:53:15 GMT -->
 <!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
   <meta charset="utf-8" />
